8f85ff54335a831ca0d1d4653a997139397b81f8
SeanVisser1998 <32290279+SeanVisser1998@users.noreply.github.com>
2018-09-13 20:18:10 +0200

    Opgave 10 <3

---
+++ School/Week2/Opgave10.py
@@ -0,0 +1,24 @@
+'''
+Created on 13 Sep 2018
+
+@author: seanv
+'''
+import matplotlib.pyplot as plt
+from collections import Counter
+from string import ascii_lowercase
+
+astronaut_text = open(r"C:\Users\seanv\Desktop\astronaut.txt", 'r')
+
+
+def countLetters(text):
+    with text as f:
+        return Counter(letter for line in f
+                for letter in line.lower()
+                if letter in ascii_lowercase)
+        
+Data = countLetters(astronaut_text)
+plt.bar(range(len(Data)), list(Data.values()), align='center')
+plt.xticks(range(len(Data)), list(Data.keys()))
+
+plt.show()
+
