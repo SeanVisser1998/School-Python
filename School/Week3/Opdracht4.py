a0fc1b205c2cc97f6ad1e8fa70c09c5c7d2b302e
Sean <s.l.visser@st.hanze.nl>
2018-09-25 18:57:54 +0200

    Roman to int =)

--- School/Week3/Opdracht4.py
+++ School/Week3/Opdracht4.py
@@ -9,20 +9,39 @@ Created on 19 Sep 2018
 '''
 
 #Class Roman
-class roman():
-    
-    #Dictionary de we gaan gebruiken om romeinse 'cijfers' aan onze cijfers te koppelen
-    rom_value= {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+class Roman():
+
     
     #Initialisatie van de klasse
-    def __init__(self, csl, s):
-        self.csl = csl
-        self.s = s
+    def __init__(self):
+        self = self #initializatie van classe
     
-    def roman_to_int(self, csl, s):
-        self.csl = csl
-        self.s = s
-        print(s)
-        print(csl)
+    #Classe die aan de hand van de dictionary Roman omzet naar Cijfers
+    ''' 
+        Returned: Cijfer
+        Geeft de waarde van de romeinse cijfers in onze cijfers terug
         
-Integer = roman('X','X')
+        Functie die aan de hand van de dictionary de romeinse cijfers vertaald naar onze cijfers
+    '''
+    def roman_to_int(self, roman):
+        
+           
+        #Dictionary de we gaan gebruiken om romeinse 'cijfers' aan onze cijfers te koppelen
+        rom_val= {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        
+        int_value = 0 #Zet de waarde van de integer (terug) op 0
+        for i in range(len(roman)): #Blijft i ophogen tot het gelijk is aan de lengte van de roman
+            if i > 0 and rom_val[roman[i]] > rom_val[roman[i-1]]: #Als i groter is dan 0 EN de rom_value[roman[i]] is GROTER DAN rom_val[roman[is]], DAN....
+                int_value += rom_val[roman[i]] - 2 * rom_val[roman[i-1]]
+            else:
+                int_value += rom_val[roman[i]] #Int_value wordt met rom_val opgehoogd
+        
+        return int_value #Returned int_value
+    
+    
+roman = Roman()
+print(roman.roman_to_int('MMXVIII'))
+                
+    
+    
+        
