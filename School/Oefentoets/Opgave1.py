fce532dbbdf242e3867585528c67846d72b05405
SeanVisser1998 <32290279+SeanVisser1998@users.noreply.github.com>
2018-10-07 20:25:55 +0200

    hex naar dec ook toegevoegd, want je weet maar nooit ;)

--- School/Oefentoets/Opgave1.py
+++ School/Oefentoets/Opgave1.py
@@ -37,3 +37,26 @@ def main():
     assert to_hex(12345) == '3039' 
     #assert to_hex(0) == '0' 
 main()
+
+
+
+
+
+'''
+    Hexadecimaal naar decimal conversie, want ja... U never know of dit op de toets komt xd
+'''
+def decNummer(digit):
+    digits = ['0', '1', '2','3', '4', '5','6', '7', '8','9', 'A', 'B','C', 'D', 'E','F']
+    for x in range(len(digits)):
+        if digit == digits[x]:
+            return x
+        
+def hexToDec(hexNum):
+    decNum = 0
+    power = 0
+    for digit in range(len(hexNum), 0, -1):
+        decNum = decNum + 16 ** power * decNummer(hexNum[digit-1])
+        power += 1
+    return decNum
+
+print(hexToDec("FF"))
